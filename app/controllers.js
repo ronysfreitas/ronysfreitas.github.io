ca8f7dc20ae3cdabc9b3a2c6aeaf23db583115e5
Rony Freitas <ronyfreitas98@gmail.com>
2017-10-16 13:43:14 -0200

    Facebook Comments

--- app/controllers.js
+++ app/controllers.js
@@ -34,30 +34,17 @@ app.controller('AppCtrl', function($http, $scope)
     });
 });
 
-app.controller('ViewMDCtrl', function($http, $stateParams)
+app.controller('ViewMDCtrl', function($http, $stateParams, $scope)
 {
+    $scope.getUrl = function(){
+        return  window.location.href;
+    }
+
     $http.get('contents/' + $stateParams.urlMD + '.md').then(function (response) {
         var converter = new showdown.Converter();
         var html = converter.makeHtml(response.data);
-    
+
         document.getElementById("viewMD").innerHTML = html;
-
-        var url_atual = window.location.href;
-        
-            alert(url_atual);
-            
-            var disqus_config = function () {
-                this.page.url = url_atual;
-                this.page.identifier = url_atual;
-            };
-                    
-            var d = document, s = d.createElement('script');
-            s.src = 'https://https-ronysfreitas-github-io.disqus.com/embed.js';
-            s.setAttribute('data-timestamp', + new Date());
-            (d.head || d.body).appendChild(s);
-            
     });
-
     
-
 });
