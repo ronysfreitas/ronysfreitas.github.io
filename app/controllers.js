7c0c42c8fc76056af8e08c14a6c44208970b7049
Rony Freitas <ronyfreitas98@gmail.com>
2017-10-16 11:42:53 -0200

    Update Disqus

--- app/controllers.js
+++ app/controllers.js
@@ -41,5 +41,23 @@ app.controller('ViewMDCtrl', function($http, $stateParams)
         var html = converter.makeHtml(response.data);
     
         document.getElementById("viewMD").innerHTML = html;
+
+        var url_atual = window.location.href;
+        
+            alert(url_atual);
+            
+            var disqus_config = function () {
+                this.page.url = url_atual;
+                this.page.identifier = url_atual;
+            };
+                    
+            var d = document, s = d.createElement('script');
+            s.src = 'https://https-ronysfreitas-github-io.disqus.com/embed.js';
+            s.setAttribute('data-timestamp', + new Date());
+            (d.head || d.body).appendChild(s);
+            
     });
+
+    
+
 });
